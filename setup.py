1a4bed647ab6811392e08cd9f5e6e53672236227
Jeasine Ma <jeasinema@gmail.com>
2021-03-10 15:28:34 -0800

    ...

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from distutils.core import setup
-setup(name='mannbaselines',
-      version='0.0.1',
-      packages=['dataset', 'memory', 'models', 'utils'],
-      )
